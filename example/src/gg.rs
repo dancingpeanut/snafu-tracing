19083a19054ab95e36546ec9b21152eef62ee5cc
zhoutongsheng <zhoutongsheng@haizhi.com>
2025-04-22 11:42:43 +0800

    update

---
+++ example/src/gg.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use crate::kk::{MyError, Result};
+
+pub fn g1() -> Result<()> {
+    let _ = File::open("test.txt")?;
+
+    Ok(())
+}
+
+pub fn g2() -> Result<()> {
+    let resp = reqwest::blocking::get("demo.haizhi.com/")?;
+
+    Ok(())
+}
--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,8 @@
 mod errors;
 mod m1;
 mod m2;
+mod kk;
+mod gg;
 
 fn main() -> errors::Result<()> {
     if let Err(e) = m1::hello_err() {
@@ -20,6 +22,13 @@ fn main() -> errors::Result<()> {
     }
     if let Err(e) = m1::hello_anyhow() {
         println!("--10: {e:?}");
+    }
+    
+    if let Err(e) = gg::g1() {
+        println!("--11: {e:?}");
+    }
+    if let Err(e) = gg::g2() {
+        println!("--12: {e:?}");
     }
 
     Ok(())
